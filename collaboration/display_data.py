707d8940e797b27033928e949b3520e179fe6026
YiWen Hon <yiwen.hon1@nhs.net>
2026-09-22 14:52:40 +0100

    Add formatted date into displayed message
    Added a function to get and format the current date.

--- collaboration/display_data.py
+++ collaboration/display_data.py
@@ -1,5 +1,10 @@
 import random
+from datetime import UTC, datetime
 
+def get_and_format_date():
+    today = datetime.now(tz=UTC)
+    date_formatted = today.strftime("%Y-%m-%d")
+    return date_formatted
 
 def select_and_display_message(valid_file_contents: dict) -> None:
     """Randomly selects one of the valid file contents and prints it to the terminal
@@ -9,5 +14,7 @@ def select_and_display_message(valid_file_contents: dict) -> None:
         filenames and the values are a dict of the parsed file contents.
     """
     selection = random.choice(list(valid_file_contents.values()))
+    date = get_and_format_date()
     print("*** Message of the day follows ***")
+    print(f"📅 Tooday is {date}.")
     print(f"{selection.get('name')} says {selection.get('message')}!")
